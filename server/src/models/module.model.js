eabd7e775ae39abaa78e46636b9c6077bbad1506
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2025-12-09 23:15:35 +0200

    Created mongo db schemas for the storage of the modules

--- server/src/models/module.model.js
+++ server/src/models/module.model.js
@@ -1,6 +1,46 @@
 import mongoose from 'mongoose'
-const { schema } = mongoose;
+const { Schema } = mongoose;
 
-const moduleSchema = new Schema ({
-    
+const SessionSchema = new Schema({
+  day: {
+    type: String,
+    required: true,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+  },
+  startTime: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    default: 1
+  },
+  location: String
 });
+
+const ActivityGroupSchema = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  groupName: {
+    type: String,
+    required: true
+  },
+  sessions: [SessionSchema] 
+});
+
+const ModuleSchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  activityGroups: [ActivityGroupSchema] 
+});
+
+module.exports = mongoose.model('Module', ModuleSchema);
